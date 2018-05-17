836b792c7b2488d3284ae31c5f66eb74cdc9469c
Sajwanankita <31779268+Sajwanankita@users.noreply.github.com>
2018-05-17 17:19:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 "use strict";
-/**
- * @author Ankita Kumari
- */
+/* Ankita Kumari */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path = __importDefault(require("path"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
-
-app.use("/", express.static(path.join(__dirname, "dist")));
-
-app.listen(process.env.PORT || 8000, () => {
-    console.log("Server is running at http://localhost:8000");
-});
+app.use(express_1.default.urlencoded({
+    extended: true
+}));
+var port = process.env.PORT || 3000;
+app.use('/', express_1.default.static(path_1.default.join(__dirname, 'dist')));
+app.listen(port, () => console.log('Server started at http://localhost:'+port));
