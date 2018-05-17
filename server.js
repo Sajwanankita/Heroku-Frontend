179be66223e2b823bacc476222840abb3125c167
Sajwanankita <ankitasajwan123@gmail.com>
2018-05-17 17:04:06 +0530

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * @author Ankita Kumari
+ */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Server is running at http://localhost:8000");
+});
