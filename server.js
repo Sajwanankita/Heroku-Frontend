6d966ec967f22c44abdca97fe069377666d2e6ab
Sajwanankita <31779268+Sajwanankita@users.noreply.github.com>
2018-05-17 17:13:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path = __importDefault(require("path"));
 const app = express_1.default();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+
+app.use("/", express.static(path.join(__dirname, "dist")));
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("Server is running at http://localhost:8000");
